242a0128dd61fa6ddab8d9e9d8caa427c4b7cf00
Yusuf Tufan <ytfn7272@gmail.com>
2025-07-08 16:58:44 +0300

    The application codes have been updated.

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,66 +1,61 @@
 import random
 import time
+from tkinter import *
 
-print("""
------------------------------
-WELCOME TO THE GAME!
-You have 5 guesses.
-Enter 'q' or 'Q' for EXİST
------------------------------
+window=Tk()
+window.minsize(700,700)
+window.config(pady=50)
 
-""")
+list_num=[]
 
-guess_right=5
-
-while True:
-    if guess_right == 0:
-        break
-    num_range=input("Which number from 0 would you like to predict?: ")
-
-    if num_range== 'q' or num_range=='Q':
-        print("The Game is Ending...")
-        time.sleep(1)
-        break
+def range_num():
 
     try:
-        num_range =int(num_range)
-    except ValueError:
-        print("Please enter only one number.")
-        continue
+        user_range=int(enter_range.get())
+        num = random.randint(0, user_range)
+        list_num.clear()
+        list_num.append(num)
 
-    num=random.randint(0,num_range)
+    except:
 
-    while True:
-        if guess_right==0:
-            print(f"Your rights are over\nThe Game is Ending...\nThe number was: {num}")
-            time.sleep(1)
-            break
+        error_lbl=Label(text='Just enter a number!', width=20, height=6, bg='light green', fg='black',font=('Arial', 10, 'bold'))
+        error_lbl.place(x=260, y=240)
 
-        user_guess=input("What is your guess?:")
-        try:
-            user_guess=int(user_guess)
-        except ValueError:
-            print("Please enter only one number.")
-            continue
 
-        if user_guess == 'q' or user_guess== 'Q':
-            print("The Game is Ending...")
-            time.sleep(1)
-            break
-        elif num == user_guess:
-            print(f"Congratulations on your correct guess\nThe number was:{num} ")
-            guess_right-=1
-            print("New round\nPress 'q' or 'Q' to exit.")
-            break
-        elif num > user_guess:
-             print("Increase your number!")
-             guess_right-=1
-             continue
-        elif num < user_guess:
-            print("Reduce your number!")
-            guess_right-=1
-            continue
+def guess_num():
+    try:
+        user_input=int(enter_num.get())
+        if user_input == list_num[0]:
+            lbl_num = Label(text=f'True Guess\nNumber was {list_num[0]}\nShuffle the numbers\nto play again.', width=20, height=6, bg='light green', fg='black',font=('Arial', 10, 'bold'))
+            lbl_num.place(x=260, y=240)
         else:
-            print("Wrong command...")
-            guess_right -=1
-            continue
+            lbl_error = Label(text='Wrong Number\nTry another number.', width=20, height=6, bg='light green', fg='black',font=('Arial', 10, 'bold'))
+            lbl_error.place(x=260, y=240)
+    except:
+        error_lbl=Label(text='Just enter a number!', width=20, height=6, bg='light green', fg='black',font=('Arial', 10, 'bold'))
+        error_lbl.place(x=260, y=240)
+
+
+#lbl enter (0,input)
+lbl_numberrange=Label(text='Guess between 0 and the number you write.',font=('Arial',15,'bold'))
+lbl_numberrange.place(x=140,y=0)
+#enter range
+enter_range=Entry(width=10,bg='orange',fg='black',font=('Arial',15,'bold'))
+enter_range.place(x=260,y=35)
+
+#button range
+range_button=Button(text='Shuffle Number.',width=15,bg='light blue',command=range_num)
+range_button.place(x=260,y=75)
+
+
+#lbl enter num
+lbl_enter=Label(text='Enter Your Guess',font=('Arial',15,'bold'))
+lbl_enter.place(x=230,y=125)
+#enter num
+enter_num=Entry(width=10,bg='orange',fg='black',font=('Arial',15,'bold'))
+enter_num.place(x=260,y=150)
+#button guess
+guess_button=Button(text='Guess',width=15,bg='light blue',command=guess_num)
+guess_button.place(x=260,y=190)
+
+window.mainloop()
